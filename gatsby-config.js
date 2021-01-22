1d137d8b9d595c83b840636b955440d11f44c352
Jimmy Hong <jimmy@foreflight.com>
2021-01-22 21:57:36 +0900

    add gh-pages script

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `C&L`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@deokpyo`,
   },
   plugins: [
     "gatsby-plugin-postcss",
@@ -28,6 +28,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
